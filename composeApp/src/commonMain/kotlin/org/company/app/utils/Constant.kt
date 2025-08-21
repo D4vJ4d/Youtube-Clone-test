af7b4257bbbf74c9064e0220651b4c59667d1633
D4vJ4d <128957223+D4vJ4d@users.noreply.github.com>
2025-08-21 11:26:16 +0200

    Update Constant.kt

--- composeApp/src/commonMain/kotlin/org/company/app/utils/Constant.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/utils/Constant.kt
@@ -2,7 +2,7 @@ package org.company.app.utils
 
 object Constant {
     const val BASE_URL = "https://www.googleapis.com/youtube/v3/"
-    const val API_KEY = "AIzaSyATK5cfxRwEFXlp73Su6HrExL5_6Z0puYw"
+    const val API_KEY = "AIzaSyAqTC6v82sjgh6HqVcMUHTM_gmHyLil3ME"
     const val TIMEOUT = 10000L
     const val VIDEO_URL = "https://www.youtube.com/watch?v="
 }
